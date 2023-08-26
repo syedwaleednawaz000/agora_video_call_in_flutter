5033063e77d5965e976f67cd2ea2ef0394dda34b
syedwaleednawaz000 <syedwaleednawaz333@gmaile.com>
2023-08-26 12:02:34 +0500

    agora video calling in flutter

--- lib/Utils/appcontant.dart
+++ lib/Utils/appcontant.dart
@@ -1,4 +1,4 @@
 class AppConstant{
-  static String appID = "7657de882808424184039d221276aa7a";
-  static String appUrl = "https://agora-node-tokenserver.syed-waleedwal5.repl.co/rtc/test/publisher/uid/";
+  static String appID = "";
+  static String appUrl = "";
 }
